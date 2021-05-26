cfe54975dafbebeddd6d21c2f27480d051b79a14
FJ-Riveros <fran.etnies@gmail.com>
2021-05-26 11:29:20 +0200

    Adding the sort select to order the cards

--- src/main/webapp/js/desencadenadorLogica.js
+++ src/main/webapp/js/desencadenadorLogica.js
@@ -1,12 +1,13 @@
 import { aplicaEventListennersYFiltros } from "./aplicaEventsYFiltros.js";
 import { generadorCard } from "./adjuntaTarjetas.js";
-
+import {selectSeleccionado} from "./ordenacionCards.js";
 $().ready(() => {
   //Esta funcion aplica los Event Listenners y filtros a todos los campos del form
   aplicaEventListennersYFiltros();
   //Cargamos directamente las tarjetas de la BDD
   generadorCard();
-  
+
+  selectSeleccionado();
   /*Aplica el listenner al switch de cambio de tema*/
   $("#chk").change(() =>{
     document.body.classList.toggle('dark');
--- src/main/webapp/js/ordenacionCards.js
+++ src/main/webapp/js/ordenacionCards.js
@@ -1,7 +1,20 @@
 
+let localStorage= window.localStorage;
+
+//Cada vez que se visualice la web recordará la selección que se hizo de orden
+export function selectSeleccionado(){
+	let elementos = document.getElementsByTagName("option");
+	for(let i=0; i<elementos.length; i++){
+		if(elementos[i].value == localStorage.getItem("order")){
+			elementos[i].setAttribute("selected", "");
+		}
+	}
+}
+
 export function ordenacion(classId) {
   $(classId).change(function(){
-	console.log($(this).find("option:selected").attr("value"));
+	localStorage.setItem("order", $(this).find("option:selected").attr("value"))
+	
 });
 	
 	
