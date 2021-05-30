0ba517ac1ebd698ac2d1380ce463ef53814db64c
FJ-Riveros <fran.etnies@gmail.com>
2021-05-30 13:36:35 +0200

    Adding the shopping cart logic

--- src/main/webapp/js/addArticuloCarrito.js
+++ src/main/webapp/js/addArticuloCarrito.js
@@ -1,10 +1,17 @@
 import {devuelveArticulo} from "./AJAX.js";
+import {
+  adjuntaError,
+  eliminaError,
+  campoCorrecto,
+} from "./modificadoresVisualesCampos.js";
 
-export async function addArticuloCarrito(id){	
+export async function addArticuloCarrito(id){
+	$(".modal-content").html();	
 	let info = await devuelveArticulo(id);
 	adjuntaContent(info);
+	eventListennerInput(info);
+	//Adjuntamos el cuerpo de la tarjeta
 	function adjuntaContent(){
-		//Convertir en form para mandar la request directamente
 		let insertar = `
 		<div class="modal-header">
           <h1 class="modal-title" id="exampleModalLabel">${info.nombre}</h1>
@@ -16,16 +23,31 @@ export async function addArticuloCarrito(id){
                 <p>${info.descripcion}</p>
 			  </div>
 			  <div class="col-4 alineacionRight">
-                <input type="number" class="w-50" value="1">
+                <input type="number" class="w-50 inputAddCarrito">
 			  </div>
 		  	</div>
 		  <p class="mb-0">${info.precio}€/Unidad<p>
 		  <p class="mb-0">${info.existencia} Unidades disponibles<p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-          <button type="button" class="btn btn-primary">Añadir</button>
+          <button type="button" class="btn btn-secondary cerrarArticulo" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-primary" id="añadirArticuloCarrito">Añadir</button>
         </div>`;
 		$(".modal-content").html(insertar);
+	}
+	
+	//Listenner para comprobar el número introducido del usuario
+	function eventListennerInput(info){
+		$("#añadirArticuloCarrito").click(function (){	
+			if($(".inputAddCarrito")[1].value > info.existencia){
+			  adjuntaError(".inputAddCarrito", "No tenemos esa cantidad de producto")
+			}else if($(".inputAddCarrito")[1].value <= 0){
+			  adjuntaError(".inputAddCarrito", "No puedes añadir menos de 1")
+			}else{
+			  $(".cerrarArticulo").click();
+			  eliminaError(".inputAddCarrito");
+			  campoCorrecto(".inputAddCarrito");
+			}
+		});
 	}
 }
